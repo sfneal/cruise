7bcb1c11ae82f8f927bdc7e611dd9176bc56c532
sfneal <stephen.neal14@gmail.com>
2024-06-13 13:56:07 -0400

    ADD 'downstream' GitHub action for testing application installation on a boilerplate app

---
+++ cruise-app-test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e -u
+
+CRUISE_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+cd ../
+
+rm -rf laravel-boilerplate-master
+
+wget https://github.com/sfneal/laravel-boilerplate/archive/master.zip
+unzip master.zip
+rm master.zip
+
+cd laravel-boilerplate-master
+
+composer config minimum-stability dev
+
+composer config repositories.0 '{"type": "vcs", "url": "https://github.com/sfneal/cruise"}'
+
+composer require "sfneal/cruise dev-${CRUISE_BRANCH}"
+
+php artisan cruise:install
+
+composer start-test
