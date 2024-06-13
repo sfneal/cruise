dfd9354331bf3753725d05e43010b4fabdff01b1
sfneal <stephen.neal14@gmail.com>
2024-06-13 16:12:33 -0400

    try env if statement syntax

--- scripts/runners/build.sh
+++ scripts/runners/build.sh
@@ -18,7 +18,7 @@ set -e -u
 export BRANCH
 
 # Build new containers
-if [ $ENV == 'prod' ]; then
+if [ "${ENV}" == 'prod' ]; then
     # Retrieve the version number
     VERSION="$(head -n 1 version.txt)"
 	docker build -t ${IMAGE}:"${VERSION//}" .
