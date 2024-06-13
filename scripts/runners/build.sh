6bab6df7931ccd52692c51ae97431fe6a574af46
sfneal <stephen.neal14@gmail.com>
2024-06-13 13:37:21 -0400

    wip try adding branch export inside scripts

--- scripts/runners/build.sh
+++ scripts/runners/build.sh
@@ -5,7 +5,19 @@ set -e -u
 ENV=${1-"dev"}
 
 # Export Docker image Tag
-sh $(dirname "$0")/_get_branch.sh
+inside_git_repo="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
+
+if [ "$inside_git_repo" ]; then
+	BRANCH=$(git rev-parse --abbrev-ref HEAD)
+	replace='/'
+	replacewith='-'
+	BRANCH="${BRANCH/${replace}/${replacewith}}"
+	BRANCH="${BRANCH/${replace}/${replacewith}}"
+else
+	BRANCH="default"
+fi
+
+export BRANCH
 
 # Build new containers
 docker compose -f docker-compose-${ENV}.yml build
--- scripts/runners/stop.sh
+++ scripts/runners/stop.sh
@@ -2,7 +2,19 @@
 set -e -u
 
 # Export Docker image Tag
-sh $(dirname "$0")/_get_branch.sh
+inside_git_repo="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
+
+if [ "$inside_git_repo" ]; then
+	BRANCH=$(git rev-parse --abbrev-ref HEAD)
+	replace='/'
+	replacewith='-'
+	BRANCH="${BRANCH/${replace}/${replacewith}}"
+	BRANCH="${BRANCH/${replace}/${replacewith}}"
+else
+	BRANCH="default"
+fi
+
+export BRANCH
 
 docker compose down -v --remove-orphans
 
