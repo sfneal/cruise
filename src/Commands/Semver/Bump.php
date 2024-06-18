a6572c24096e77c469acaf49c9c56157a303ef9c
sfneal <stephen.neal14@gmail.com>
2024-06-18 13:29:43 -0400

    ADD custom prompt for missing argument with choices

--- src/Commands/Semver/Bump.php
+++ src/Commands/Semver/Bump.php
@@ -5,9 +5,12 @@ namespace Sfneal\Cruise\Commands\Semver;
 use Illuminate\Console\Command;
 use Illuminate\Contracts\Console\PromptsForMissingInput;
 use Illuminate\Support\Facades\Process;
+use function Laravel\Prompts\search;
 
 class Bump extends Command implements PromptsForMissingInput
 {
+    const TYPES = ['major', 'minor', 'patch'];
+
     /**
      * The name and signature of the console command.
      *
@@ -43,7 +46,11 @@ class Bump extends Command implements PromptsForMissingInput
     protected function promptForMissingArgumentsUsing(): array
     {
         return [
-            'bump' => ['Which semver segment would you like to bump?', 'E.g. major, minor or patch'],
+            'user' => fn () => search(
+                label: 'Which semver segment would you like to bump?',
+                options: fn ($type) => self::TYPES,
+                placeholder: 'E.g. major, minor or patch',
+            ),
         ];
     }
 
