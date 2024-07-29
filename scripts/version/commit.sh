8d9b0ceb44a75a8cf37b2a47b1241fc7a5c29afa
sfneal <stephen.neal14@gmail.com>
2024-07-29 15:07:01 -0400

    FIX typo

--- scripts/version/commit.sh
+++ scripts/version/commit.sh
@@ -3,4 +3,4 @@
 message=${1}
 
 # Commit to git
-git commit -m "${message}" ./version.txt ./docker-compose.yml ./docker-compose-dev.yml ./docker-compose-dev-db.yml ./docker-compose-dev-node.yml ./docker-compose-dev-tests.yml
+git commit -m "${message}" ./version.txt ./docker-compose.yml ./docker-compose-dev.yml ./docker-compose-dev-db.yml ./docker-compose-dev-node.yml ./docker-compose-tests.yml
